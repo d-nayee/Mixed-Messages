4ee66f95cd5959d5182bfb81eb13abbb3ddc475d
d-nayee <87990270+d-nayee@users.noreply.github.com>
2021-09-23 18:41:10 +0100

    moved code for selectNoun function to global scope

--- messageGenerator.js
+++ messageGenerator.js
@@ -13,52 +13,39 @@ const selectAdjectives = () => {
     randomAdjectives.push(tempAdjective)
 }
 
-const dearDiary = () => {
- selectAdjectives()
+// This function selects a random noun and pushes it to the "randomNouns" array IF the value is UNIQUE.
+// it does this 3 times.
 
-    
-
-    
-    
-    
-    
-
-
+const selectNoun = () => {
     // creating an empty list to store the value of the "selectNoun" function.
-
     const randomNouns = [];
-
-    // This function selects a random noun and pushes it to the "randomNouns" array IF the value is UNIQUE.
-    // it does this 3 times.
-
-    const selectNoun = () => {
-        let uniqueNounCheck = 0
+    let uniqueNounCheck = 0
         
         
-        // We only want an array of 3 elements.
-        while (randomNouns.length < 3) {
-            // stores a randomly generated value in variable called tempNoun
-            let tempNoun = nouns[Math.floor(Math.random() * nouns.length)]
+    // We only want an array of 3 elements.
+    while (randomNouns.length < 3) {
+        // stores a randomly generated value in variable called tempNoun
+        let tempNoun = nouns[Math.floor(Math.random() * nouns.length)]
             
 
-            // A check to see if this is the first generated random value.
-            // if it is it can be added to the selectNoun array without camparing against elements in that same array.
+        // A check to see if this is the first generated random value.
+        // if it is it can be added to the selectNoun array without camparing against elements in that same array.
 
-            if (randomNouns.length === 0) {
-                randomNouns.push(tempNoun)
+        if (randomNouns.length === 0) {
+            randomNouns.push(tempNoun)
                 
-            } else {
-                // A check against each element in the array for uniqueness.
-                randomNouns.forEach(item => {
+        } else {
+            // A check against each element in the array for uniqueness.
+            randomNouns.forEach(item => {
 
-                    if (item !== tempNoun) {
-                        uniqueNounCheck += 1
+                if (item !== tempNoun) {
+                    uniqueNounCheck += 1
                     }
                 })
 
-                // If the tempNoun is unique the uniqueNounCheck and the length of the array would be equal.
-                if(uniqueNounCheck === randomNouns.length) {
-                    randomNouns.push(tempNoun)
+            // If the tempNoun is unique the uniqueNounCheck and the length of the array would be equal.
+            if(uniqueNounCheck === randomNouns.length) {
+                randomNouns.push(tempNoun)
                 }
                 // reset the variable for the next check.
                 uniqueNounCheck = 0
@@ -67,8 +54,13 @@ const dearDiary = () => {
         } 
 
     }
-    selectNoun();
-    console.log(randomNouns)
+
+
+
+const dearDiary = () => {
+ selectAdjectives()
+ selectNoun()
+
 }
 
 dearDiary();
